42e750fdc97b18a39192e60c77095281f8efdd29
Bartimeyss <ichigvintsev@icloud.com>
2024-11-06 18:39:31 +0500

    Solved

--- App/PostfixCalculator.cs
+++ App/PostfixCalculator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,7 +8,52 @@ namespace App
     {
         public static string Calculate(string postfixExpression)
         {
-            throw new NotImplementedException();
+            if (postfixExpression == null) throw new FormatException();
+            if (postfixExpression.Length == 0) return "0";
+            var postfixExpressionArray = postfixExpression.Split(' ');
+            if (postfixExpression.Split(' ').Length == 1)
+            {
+                if (!(char.IsNumber(postfixExpression[0]) || postfixExpression[0] == '-'))
+                {
+                    throw new FormatException();
+                }
+                return postfixExpression;
+            }
+            
+            var stack = new Stack<int>();
+            for (var i = 0; i < postfixExpressionArray.Length; i++)
+            {
+                postfixExpressionArray[i] = postfixExpressionArray[i].Trim();
+                if (int.TryParse(postfixExpressionArray[i], out int number))
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    if (stack.Count < 2) throw new FormatException();
+                    int b = stack.Pop();
+                    int a = stack.Pop();
+                    switch (postfixExpressionArray[i])
+                    {
+                        case "+":
+                            stack.Push(a + b);
+                            break;
+                        case "-":
+                            stack.Push(a - b);
+                            break;
+                        case "*":
+                            stack.Push(a * b);
+                            break;
+                        case "/":
+                            stack.Push(a / b);
+                            break;
+                        default:
+                            throw new FormatException();
+                    }
+                }
+            }
+            if (stack.Count != 1) throw new FormatException();
+            return stack.Pop().ToString() ?? "0";
         }
     }
 }
